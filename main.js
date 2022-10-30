44bb3b91af8fa03afed00c5cb259dacb6cfd6494
Noriyuki Hamada <hamada@jyohokikaku.co.jp>
2022-10-30 12:34:52 +0900

    マウス操作

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 import * as THREE from './build/three.module.js';
 import { FlyControls } from './jsm/controls/FlyControls.js';
+
+const windouWD = window.innerWidth;
+const windouHT = window.innerHeight;
+const clock = new THREE.Clock();
+
+//シーン
+const scene = new THREE.Scene();
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+	40, //1st 40:視野角
+	windouWD / windouHT, //2nd アスペクト比
+	1, //3rd 開始距離
+	15000 //4th 終了距離
+);
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+
+//コントロール
+const controls = new FlyControls(camera, renderer.domElement);
+controls.movementSpeed = 2500;
+controls.rollSpeed = Math.PI / 90;
+
+const animate = () => {
+	requestAnimationFrame(animate);
+	const delta = clock.getDelta(); //経過時間
+	controls.update(delta);
+	renderer.render(scene, camera);
+};
+
 const init = () => {
 	console.log('init');
-
-	const clock = new THREE.Clock();
-	const windouWD = window.innerWidth;
-	const windouHT = window.innerHeight;
-
 	//カメラ
-	const camera = new THREE.PerspectiveCamera(
-		40, //1st 40:視野角
-		windouWD / windouHT, //2nd アスペクト比
-		1, //3rd 開始距離
-		15000 //4th 終了距離
-	);
 	camera.position.z = 250;
-
-	//シーン
-	const scene = new THREE.Scene();
-
 	//geometry
 	const boxSize = 250;
 	const geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
@@ -51,13 +65,11 @@ const init = () => {
 	addLight(0.08, 0.3, 0.9, 0, 0, -1000);
 
 	//レンダラー
-	const renderer = new THREE.WebGLRenderer();
-
-	// controls = new FlyControls(camera, renderer.domElement);
-
 	renderer.setSize(windouWD, windouHT);
 	document.body.appendChild(renderer.domElement);
 	renderer.render(scene, camera);
+
+	animate();
 };
 
 init();
